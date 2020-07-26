80303aebc5271887fa4f1dec1394e5bc345032ed
Valentin <stamatevalentin125@gmail.com>
2020-07-26 09:01:36 +0300

    a beta version of materials component

--- src/Components/Materials/Materials.tsx
+++ src/Components/Materials/Materials.tsx
@@ -8,28 +8,24 @@ const Materials = () => {
             <div className="mt-component container-full-dark ">
 
                 <ButtonGroup aria-label="Basic example">
-                    <Button variant="outline-light">First Year</Button>
-                    <Button variant="outline-light">Second Year</Button>
-                    <Button variant="outline-light">Third Year</Button>
+                    <Button variant="outline-info">First Year</Button>
+                    <Button variant="outline-warning">Second Year</Button>
+                    <Button variant="outline-success">Third Year</Button>
                 </ButtonGroup>
 
                 <Row>
                     <Col>
 
-                        <Card>
+                        <Card style={{backgroundColor: "#404040"}}>
                             <ListItem title="Matematica" action="http://google.com" />
                         </Card>
 
                     </Col>
                     <Col>
 
-                        <ListGroup>
-                            <ListGroup.Item variant="light">Cras justo odio</ListGroup.Item>
-                            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-                        </ListGroup>
+                        <Card style={{backgroundColor: "#404040"}}>
+                            <ListItem title="Programare Orientata Obiect" action="http://google.com" />
+                        </Card>
 
                     </Col>
                 </Row>
