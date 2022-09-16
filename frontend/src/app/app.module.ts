d3d5a65422e1b40563a0152f0577dd197f2138b4
Valentin Stamate <stamatevalentin125@gmail.com>
2022-09-16 22:46:25 +0300

    change ui library to tailwind

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,24 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './pages/navbar/navbar.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { NavbarComponent } from './widgets/navbar/navbar.component';
-import { MoreMaterialsComponent } from './widgets/more-materials/more-materials.component';
-import { MaterialsComponent } from './widgets/materials/materials.component';
+import { MaterialsComponent } from './pages/materials/materials.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
     NavbarComponent,
-    MoreMaterialsComponent,
+    LandingPageComponent,
     MaterialsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
