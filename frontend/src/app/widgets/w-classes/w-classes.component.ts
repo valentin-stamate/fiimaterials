ef99d4e4cb82bfde242d0eabb107633ccebe63e3
Valentin Stamate <stamatevalentin125@gmail.com>
2023-02-11 22:51:49 +0200

    Classes widget

---
+++ frontend/src/app/widgets/w-classes/w-classes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-w-classes',
+  templateUrl: './w-classes.component.html',
+  styleUrls: ['./w-classes.component.scss']
+})
+export class WClassesComponent {
+
+}
